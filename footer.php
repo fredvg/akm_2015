e8fac0afe150b302b9e55e6bb7ccd4238dcf3ee4
madebyfred <fred@madebyfred.com>
2014-11-23 17:11:47 -0500

    added sass support

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'akm_2015' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'akm_2015' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'akm_2015' ), 'AKM_2015', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'akm_2015' ), 'AKM_2015', '<a href="http://madebyfred.com" rel="designer">Made By Fred</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
